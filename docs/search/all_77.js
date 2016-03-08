348f8e007e1231a360fbf5ae5039ed4c99ef4d5f
Travis <travis@travis-ci.org>
2016-03-08 03:27:32 +0000

    Travis build 48 pushed to gh-pages

--- docs/search/all_77.js
+++ docs/search/all_77.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['w',['W',['../unionword.html#a2f376e9fd3eca4a8ec08b269f764ad42',1,'word']]],
   ['word',['word',['../unionword.html',1,'']]]
 ];
--- docs/search/functions_5f.js
+++ docs/search/functions_5f.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['_5fload_5fmem_5fidx_5foffset_5freg8',['_load_mem_idx_offset_reg8',['../z80_8c.html#af15ac3030d787c91b40958824bbee38d',1,'_load_mem_idx_offset_reg8(uint8_t *reg, word *index_register, uint8_t *memory, word *pc):&#160;z80.c'],['../z80_8h.html#af15ac3030d787c91b40958824bbee38d',1,'_load_mem_idx_offset_reg8(uint8_t *reg, word *index_register, uint8_t *memory, word *pc):&#160;z80.c']]],
+  ['_5fload_5freg16_5fnn',['_load_reg16_nn',['../z80_8c.html#a1996a16c481738563ee9664c86798310',1,'z80.c']]],
   ['_5fload_5freg8_5fmem_5fidx_5foffset',['_load_reg8_mem_idx_offset',['../z80_8c.html#a20869ce2e719e74335cde7a9ff68da72',1,'_load_reg8_mem_idx_offset(uint8_t *reg, word *index_register, uint8_t *memory, word *pc):&#160;z80.c'],['../z80_8h.html#a20869ce2e719e74335cde7a9ff68da72',1,'_load_reg8_mem_idx_offset(uint8_t *reg, word *index_register, uint8_t *memory, word *pc):&#160;z80.c']]],
   ['_5fload_5freg8_5fmem_5fpair',['_load_reg8_mem_pair',['../z80_8c.html#a98200db180fcce4a6df8cf3fe6ff38a4',1,'_load_reg8_mem_pair(uint8_t *reg, word *address_pair, uint8_t *memory):&#160;z80.c'],['../z80_8h.html#a98200db180fcce4a6df8cf3fe6ff38a4',1,'_load_reg8_mem_pair(uint8_t *reg, word *address_pair, uint8_t *memory):&#160;z80.c']]]
 ];
