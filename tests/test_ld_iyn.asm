ee4b999091bff84eecf064f5eeee5f22a406e08e
Peter H. Ezetta <protocall7@gmail.com>
2016-03-10 22:23:48 -0800

    adding test bins

---
+++ tests/test_ld_iyn.asm
@@ -0,0 +1,17 @@
+ld iy,0x040
+ld (iy+0),0x00
+ld (iy+1),0x01
+ld (iy+2),0x02
+ld (iy+3),0x03
+ld (iy+4),0x04
+ld (iy+5),0x05
+ld (iy+6),0x06
+ld (iy+7),0x07
+
+ld a,(iy+1)
+ld b,(iy+2)
+ld c,(iy+3)
+ld d,(iy+4)
+ld e,(iy+5)
+ld h,(iy+6)
+ld l,(iy+7)
