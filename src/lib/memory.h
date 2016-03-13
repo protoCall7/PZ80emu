71b8f3095e8777996a49a427e154c3018b30e73f
Peter H. Ezetta <protocall7@gmail.com>
2016-03-12 19:47:00 -0800

    updating docs

--- src/lib/memory.h
+++ src/lib/memory.h
@@ -15,10 +15,18 @@
 /** Size of memory for create_ram() to allocate */
 #define MEMSIZE 65536
 
+/**
+ * z80 memory class
+ * \brief z80 memory class
+ */
 typedef struct {
+	/** Pointer to block of memory for allocation */
 	uint8_t *memory;
 
+	/** Pointer to memory_load method */
 	long (*memory_load)(void *self, const char *filename);
+
+	/** Pointer to memory_free method */
 	void (*memory_free)(void *self);
 } memory;
 
