786bd7655125f86c42721e4dc2fed43148b43a3b
Travis <travis@travis-ci.org>
2016-07-06 00:20:52 +0000

    Travis build 54 pushed to gh-pages

--- docs/search/all_6d.js
+++ docs/search/all_6d.js
@@ -1,9 +1,11 @@
 var searchData=
 [
   ['main',['main',['../_p_z80emu_8c.html#ac0f2228420376f4db7e1274f2b41667c',1,'PZ80emu.c']]],
-  ['memory',['memory',['../structmemory.html',1,'']]],
+  ['memory',['memory',['../structmemory.html',1,'memory'],['../structmemory.html#a1c52ada0af9df0cd764af60d7bcda5c8',1,'memory::memory()']]],
   ['memory_2ec',['memory.c',['../memory_8c.html',1,'']]],
   ['memory_2eh',['memory.h',['../memory_8h.html',1,'']]],
+  ['memory_5ffree',['memory_free',['../structmemory.html#aede34c1ea7eaf4f50796bde97274f804',1,'memory']]],
+  ['memory_5fload',['memory_load',['../structmemory.html#a4a91f793077b5d7df7807666a68ad437',1,'memory']]],
   ['memory_5fnew',['memory_new',['../memory_8c.html#a275b343797d96b7116c8ea15e1540a50',1,'memory_new(void):&#160;memory.c'],['../memory_8h.html#a7b2957db09ca2550636fb55c44efdf7c',1,'memory_new():&#160;memory.c']]],
   ['memsize',['MEMSIZE',['../memory_8h.html#ad4661974a66daba2cf78a74dfbe67ebf',1,'memory.h']]]
 ];
---
+++ docs/search/variables_6d.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['memory',['memory',['../structmemory.html#a1c52ada0af9df0cd764af60d7bcda5c8',1,'memory']]],
+  ['memory_5ffree',['memory_free',['../structmemory.html#aede34c1ea7eaf4f50796bde97274f804',1,'memory']]],
+  ['memory_5fload',['memory_load',['../structmemory.html#a4a91f793077b5d7df7807666a68ad437',1,'memory']]]
+];
